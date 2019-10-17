4d363662ba24494a094018260a2e82feaf4ce2d6
douglaspereira <douglas.pereira@serpro.gov.br>
2019-10-17 08:56:54 -0300

    projeto de animação de flare com flutter

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:flare_dart/actor.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 150,
+          height: 150,
+          child: FlareActor("assets/AnimGears.flr", animation: "spin1",),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(Duration(seconds: 3)).then((_){
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => Home())
+      );
+    });
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text("Flutter + Flare",
+          style: TextStyle(fontSize: 30.0),
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            child: FlareActor("assets/AnimHeart.flr", animation: "pulse",),
+          )
+        ],
+      ),
+    );
+  }
+}
+
